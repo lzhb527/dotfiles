826ffefc28bb3b11541e9eae91c65127b9803316
lzhb527 <lzhb527@126.com>
2026-05-01 19:03:24 +0800

    xfconf-query

--- media/.config/media/tools/xfce4-init.sh
+++ media/.config/media/tools/xfce4-init.sh
@@ -21,4 +21,4 @@ xfconf-query -c xfwm4 -p /general/margin_bottom -n -t int -s 8
 xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/<Super>Return" -n -t string -s "kitty"
 xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/<Super>slash" -n -t string -s "kitten quick-access-terminal"
 xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/<Super>o" -n -t string -s "ulauncher"
-xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Super>f" -n -t string -s "maximize_window_key"
+# xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Super>f" -n -t string -s "maximize_window_key"
