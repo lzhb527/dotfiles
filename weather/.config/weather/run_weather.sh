396d0bc43cbac7548dcaf9558e90cf422622b4a4
lzhb527 <lzhb527@126.com>
2026-09-23 16:28:57 +0800

    tmux

--- weather/.config/weather/run_weather.sh
+++ weather/.config/weather/run_weather.sh
@@ -6,5 +6,5 @@
 # Description：
 # Copyright (C) 2026  Ltd. All rights reserved.
 
-sh ~/.config/weather/weather-zh.sh >/tmp/weather_report.txt
-sh ~/.config/weather/weather7-zh.sh >/tmp/weather_report7.txt
+bash ~/.config/weather/weather-zh.sh >/tmp/weather_report.txt
+bash ~/.config/weather/weather7-zh.sh >/tmp/weather_report7.txt
